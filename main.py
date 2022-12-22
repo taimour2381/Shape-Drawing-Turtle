3269de82438b5f760d5a29d8d2ea9df05d929c67
taimour2381 <119933727+taimour2381@users.noreply.github.com>
2022-12-22 13:02:19 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import turtle
+tim = turtle.Turtle()
+tim.color("red")
+tim.shape("turtle")
+tim.speed(1)
+for n in range (3):
+    tim.color("black")
+    tim.forward(100)
+    tim.right(120)
+for n in range (4):
+    tim.color("orange")
+    tim.forward(100)
+    tim.right(90)
+for n in range (5):
+    tim.color("red")
+    tim.forward(100)
+    tim.right(72)
+for n in range (6):
+    tim.color("blue")
+    tim.forward(100)
+    tim.right(60)
+for n in range (8):
+    tim.color("yellow")
+    tim.forward(100)
+    tim.right(45)
+for n in range (9):
+    tim.color("purple")
+    tim.forward(100)
+    tim.right(40)
+for n in range (10):
+    tim.color("gold")
+    tim.forward(100)
+    tim.right(36)
+
+
+turtle.exitonclick()
+
